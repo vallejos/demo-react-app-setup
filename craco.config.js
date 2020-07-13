6dd9de19583f511d3da1f88f893ee742f3f800d9
Fabian Vallejos <vallejosfab@gmail.com>
2020-07-13 15:57:09 -0300

    Final version with nodemon, craco, antd, less, css modules

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,40 @@
-module.exports = {
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const WebpackBar = require("webpackbar");
+const path = require("path");
+const CracoAntDesignPlugin = require("craco-antd");
+const CracoLessPlugin = require('craco-less');
 
+module.exports = {
+  webpack: {
+    plugins: [
+      new WebpackBar({ profile: true }),
+      ...(process.env.NODE_ENV === 'development'
+        ? [new BundleAnalyzerPlugin({ openAnalyzer: false })]
+        : [])
+    ]
+  },
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeThemeLessPath: path.join(
+          __dirname,
+          "src/theme.less"
+        )
+      }
+    },
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        cssLoaderOptions: {
+          modules: { localIdentName: '[local]_[hash:base64:5]' },
+        },
+        modifyLessRule: function(lessRule, _context) {
+          lessRule.test = /\.(less)$/;
+          lessRule.exclude = path.join(__dirname, 'node_modules');
+          return lessRule;
+        },
+      },
+    },
+  ],
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.less';
+import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.App_header}>
+        <img src={logo} className={styles.App_logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type="primary">Click me!</Button>
         <a
-          className="App-link"
+          className={styles.App_link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
